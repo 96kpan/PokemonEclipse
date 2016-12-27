491175308484976fffe481d38fafdc3890f659e6
Your Name <katie@efferential.com>
2016-12-27 07:19:09 -0500

    tiles have been added

---
+++ src/tiles/BushTile.java
@@ -0,0 +1,21 @@
+package tiles;
+
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+import model.PokemonGame;
+
+public class BushTile extends Tile implements Serializable{
+
+	public BushTile(BufferedImage tileImage) {
+		super(tileImage);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void playerIsOnTile(PokemonGame game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/tiles/Tile.java
@@ -0,0 +1,40 @@
+package tiles;
+
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+import model.PokemonGame;
+
+public abstract class Tile implements Serializable {
+	//private transient BufferedImage tileImage;
+	private boolean hasTrainer;
+	private boolean hasPokemon;
+	protected boolean canMove;
+	
+	public Tile(BufferedImage tileImage) {
+		//this.tileImage = null;
+		this.hasTrainer = false;
+		this.hasPokemon = false;
+		canMove = true;
+	}
+	
+	public boolean canMove() {
+		return this.canMove;
+	}
+	
+	public boolean getHasTrainer() {
+		return hasTrainer;
+	}
+	
+	public boolean getHasPokemon() {
+		return false;
+	}
+	
+	public void setHasTrainer(boolean hasHunter) {
+		this.hasTrainer = hasHunter;
+	}
+	
+	public abstract void playerIsOnTile(PokemonGame game);
+}
+	
+
