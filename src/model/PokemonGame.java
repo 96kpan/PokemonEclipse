caeccfe92bc66335dd57def07b6f2b26fe679886
Your Name <katie@efferential.com>
2016-12-27 08:26:04 -0500

    tiles have been added

--- src/model/PokemonGame.java
+++ src/model/PokemonGame.java
@@ -3,6 +3,21 @@ package model;
 public class PokemonGame {
 	
 	private Trainer trainer;
+
+	public String getItemAdded() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void acquireItem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void launchBattle() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
--- src/tiles/SmallTreeTile.java
+++ src/tiles/SmallTreeTile.java
@@ -1,5 +1,28 @@
 package tiles;
 
-public class SmallTreeTile {
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+import model.PokemonGame;
+
+public class SmallTreeTile extends Tile implements Serializable{
+	
+
+	public SmallTreeTile(BufferedImage tileImage) {
+		super(tileImage);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean canMove() {
+		return false;
+	}
+
+	@Override
+	public void playerIsOnTile(PokemonGame game) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
